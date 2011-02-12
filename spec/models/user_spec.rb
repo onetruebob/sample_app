c3246922849119ba2caa8aa399a97d89b2aee5fa
onetruebob <bob@bobowen.org>
2011-02-12 11:15:27 -0500

    finished password items on user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -103,6 +103,48 @@ describe User do
       @user.should respond_to(:encrypted_password)
     end
     
+    it"should set the encrypted password attribute" do
+      @user.encrypted_password.should_not be_blank
+    end
+    
+    it "should have a salt" do
+      @user.should respond_to(:salt)
+    end
+    
+    describe "has password? menthod" do
+      
+      it "should exist" do
+        @user.should respond_to(:has_password?)
+      end
+      
+      it "should return true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      
+      it "should return false if the passwords don't match" do
+        @user.has_password?("NonMatchingPassword").should be_false
+      end
+      
+    end
+    
+    describe "authenticate method" do
+      it "should exist" do
+        User.should respond_to(:authenticate)
+      end
+      
+      it "should reutrn nil on email/password mismatch" do
+        User.authenticate(@attr[:email], "WrongPassword").should be_nil
+      end
+      
+      it "should return nil for an email address with no user" do
+        User.authenticate("bad@emailaddress.com",@attr[:password]).should be_nil
+      end
+      
+      it "should return the user if found and passwords match" do
+        User.authenticate(@attr[:email],@attr[:password]).should == @user
+      end
+    end
+    
   end
   
 end
